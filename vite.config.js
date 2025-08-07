82376e2dbc1d54dd54871e044724f176ab5786ac
Ras123-eyu <eyoelsitotaw2020@gmail.com>
2025-08-07 23:01:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: "/Library-management-system",
-})
+  plugins: [react(), tailwindcss()],
+  base: import.meta.env.VITE_BASE_PATH || "/Library-management-system",
+});
